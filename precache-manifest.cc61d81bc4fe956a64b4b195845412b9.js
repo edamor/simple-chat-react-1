c23c6360a49865bc9ce07d77daaed25715718581
ed louise amor <ed.amor@xlog.com>
2020-09-27 19:01:38 +0800

    Updates

---
+++ precache-manifest.cc61d81bc4fe956a64b4b195845412b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fe17dd117b3b1b71136bb8676c1ef4",
+    "url": "/simple-chat-react-1/index.html"
+  },
+  {
+    "revision": "9c77a448fe5956fd1aef",
+    "url": "/simple-chat-react-1/static/css/main.77aa4293.chunk.css"
+  },
+  {
+    "revision": "35c234db94f1f759fbd5",
+    "url": "/simple-chat-react-1/static/js/2.0c367324.chunk.js"
+  },
+  {
+    "revision": "0b0b2a69dd717d7a37b173b97bdbeb3f",
+    "url": "/simple-chat-react-1/static/js/2.0c367324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c77a448fe5956fd1aef",
+    "url": "/simple-chat-react-1/static/js/main.091d9f15.chunk.js"
+  },
+  {
+    "revision": "83aec402103bff33ab22",
+    "url": "/simple-chat-react-1/static/js/runtime-main.bf5d61c5.js"
+  }
+]);
